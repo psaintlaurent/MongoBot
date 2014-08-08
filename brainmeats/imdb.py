3a2234ec3f69aa0624aebbf04923b761b14aead1
Patrick Saint-Laurent <patrick@saint-laurent.us>
2014-08-08 14:40:31 -0400

    add imdbpy to requirements

---
+++ brainmeats/imdb.py
@@ -0,0 +1,10 @@
+from autonomic import axon, help, Dendrite
+
+class Imdb(Dendrite):
+    def __init__(self, cortex):
+        super(Turing, self).__init__(cortex)
+
+    @axon
+    def movies(self):
+
+
